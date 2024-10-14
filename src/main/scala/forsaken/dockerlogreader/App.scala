9026c820998bcebef83b4ab5eded14e111eeec7f
Petros Siatos <apollo.forsaken@gmail.com>
2024-10-14 18:40:34 +0300

    Add Cats.Show, scalafmt

--- src/main/scala/forsaken/dockerlogreader/App.scala
+++ src/main/scala/forsaken/dockerlogreader/App.scala
@@ -2,8 +2,9 @@ package forsaken.dockerlogreader
 
 import forsaken.dockerlogreader.docker.DockerCommands
 import forsaken.dockerlogreader.log.LogParser
-import zio.*
+import zio._
 import zio.stream._
+import cats.implicits._
 
 object App extends ZIOAppDefault with DockerCommands:
 
@@ -21,10 +22,15 @@ object App extends ZIOAppDefault with DockerCommands:
       logParser = LogParser()
       _ <- logs(containerId).linesStream
         .map(logParser.parse)                                 // Parse each log line
-        .filter(_.isDefined)                                  // Filter out the None values
-        .tap(parsedLog => Console.printLine(parsedLog.toString))  // Print each parsed log line
-        .forever                                              // Ensure the stream runs indefinitely
-        .runDrain                                             // Consume the stream (run and ignore the result)
-        .catchAll(err => ZIO.die(err))                       // Die with the error if something goes wrong
+        .map(ZIO.fromOption)                                  // Convert to ZIO
+        .flatMap(ZStream.fromZIOOption(_))
+        .tap(parsedLog =>
+          Console.printLine(parsedLog.show)
+        ) // Print each parsed log line
+        .forever // Ensure the stream runs indefinitely
+        .runDrain // Consume the stream (run and ignore the result)
+        .catchAll(err =>
+          ZIO.die(err)
+        ) // Die with the error if something goes wrong
       _ <- ZIO.never
     yield ()
--- src/main/scala/forsaken/dockerlogreader/log/Log.scala
+++ src/main/scala/forsaken/dockerlogreader/log/Log.scala
@@ -1,11 +1,17 @@
 package forsaken.dockerlogreader.log
 
 import java.time.LocalDateTime
+import cats.Show
 
 /**
  * @author Petros Siatos
  */
 case class Log (timestamp: LocalDateTime, level: LogLevel, module: String, message: String)
+
+object Log:
+  implicit val showLog: Show[Log] = Show.show { log =>
+    s"${log.timestamp} - ${log.level} - ${log.message}"
+  }
 
 object LogExtensions:
   implicit class LogListOps(logs: List[Log]):
