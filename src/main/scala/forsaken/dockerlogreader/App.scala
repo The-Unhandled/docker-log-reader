2cb37ac3bc84f447051726ee6829da45be46d3d8
Petros Siatos <apollo.forsaken@gmail.com>
2024-10-15 13:17:17 +0300

    Add Sentry

--- src/main/scala/forsaken/dockerlogreader/App.scala
+++ src/main/scala/forsaken/dockerlogreader/App.scala
@@ -1,38 +1,61 @@
 package forsaken.dockerlogreader
 
-import cats.implicits.*
-import forsaken.dockerlogreader.docker.DockerProcesses
-import forsaken.dockerlogreader.log._
+import forsaken.dockerlogreader.docker.DockerCommands
+import forsaken.dockerlogreader.log.*
+import forsaken.dockerlogreader.log.LogLevel.ERROR
 import zio.*
 import zio.stream.*
+import io.sentry.{Sentry, SentryOptions}
+import zio.process.CommandError
 
-object App extends ZIOAppDefault with DockerProcesses:
+object App extends ZIOAppDefault with DockerCommands:
 
   def run: ZIO[Any, Throwable, Unit] =
+
+    Sentry.init(new SentryOptions {
+      setDsn(
+        "https://3a649c7bd20bf09fa70e6811605e6308@o4507940499357696.ingest.de.sentry.io/4508121477677136"
+      )
+    })
+
     // Replace with your actual container ID or name
     val containerName = "trader_abci_0"
 
     val logSink: ZSink[Any, Throwable, Log, Nothing, Unit] =
-      ZSink.foreach(log => Console.printLine(log.show))
-
-    val containerIds = list(containerName).lazyLines
+      ZSink.foreach { log =>
+        Sentry.captureException(new Exception(log.message))
+        Console.printLine("Error logged: " + log.level)
+      }
 
     // Fetch and print the logs
     for
+      containerIds <- list(containerName).lines
       containerId <- ZIO
         .fromOption(containerIds.headOption)
-        .orElseFail(new Exception("Container ID not found."))
+        .orDieWith(_ => new Exception("Container ID not found."))
       _ <- Console.printLine("Getting logs from Container ID: " + containerId)
 
-      logParser = LogParser()
-      _ <- ZStream
-        .fromIterable(logs(containerId).lazyLines)
+      _ <- logs(containerId).linesStream
+          .catchAll(err =>
+            println(s"WTF ERR + $err")
+            ZStream.succeed(s"Error line, $err")
+          )
+          .foreach(log => Console.printLine("Does this even work " + log))
+
+    /* _ <- logs(containerId).linesStream
+        .foreach(log => Console.printLine("Does this even work " + log))*/
+
+    /* logParser = LogParser()
+      _ <- logs(containerId).linesStream
+        .tap(log => Console.printLine("Does this even work " + log))
         .map(logParser.parse) // Parse each log line
         .map(ZIO.fromOption) // Convert to ZIO
         .flatMap(ZStream.fromZIOOption(_))
+        .filter(_.level == ERROR) // Filter only error logs
         .run(logSink)
         .catchAll(err =>
+          Console.printLine("Stream failed with error: " + err.getMessage) *>
           ZIO.die(err)
         ) // Die with the error if something goes wrong
-      _ <- ZIO.never // Keep the app running
+      _ <- ZIO.never // Keep the app running*/
     yield ()
--- src/test/scala/forsaken/dockerlogreader/log/LogParserSpec.scala
+++ src/test/scala/forsaken/dockerlogreader/log/LogParserSpec.scala
@@ -1,8 +1,8 @@
 package forsaken.dockerlogreader.log
 
-import forsaken.dockerlogreader.log.{LogParser, Log, LogLevel}
-import zio.test._
-import zio.test.Assertion._
+import zio.test.*
+import zio.test.Assertion.*
+
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -12,7 +12,6 @@ object LogParserSpec extends ZIOSpecDefault:
 
   def spec: Spec[Any, Nothing] = suite("LogParserSpec")(
     test("parseLog should correctly parse a valid log entry") {
-      val logParser = LogParser()
       val logEntry = "[2024-10-14 14:02:54,898] [INFO] [agent] Sharing Tendermint config on start-up?: False"
       val expectedTimestamp = LocalDateTime.parse("2024-10-14 14:02:54,898", DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss,SSS"))
       val expected = Log(expectedTimestamp, LogLevel.INFO, "agent", "Sharing Tendermint config on start-up?: False")
